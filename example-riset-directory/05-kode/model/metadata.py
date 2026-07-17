fe48eaee082eb061aef5c78677f5d48a5a7ba85c
dysaradnant <dysaradnant3@gmail.com>
2026-07-17 16:20:12 +0700

    Add files via upload

--- example-riset-directory/05-kode/model/metadata.py
+++ example-riset-directory/05-kode/model/metadata.py
@@ -1,1 +1,53 @@
+import os
+import platform
+import pandas as pd
+from datetime import datetime
 
+def save_metadata(
+    dataset_name,
+    rows,
+    period,
+    scheduler,
+    output_file
+):
+    metadata = {
+        "Parameter": [
+            "Experiment Name",
+            "Dataset",
+            "Rows",
+            "Period",
+            "Python Version",
+            "Operating System",
+            "Execution Time",
+            "PV Model",
+            "Battery Model",
+            "Energy Demand Model",
+            "Scheduler",
+            "Output File"
+        ],
+        "Value": [
+            "Adaptive Energy Scheduler",
+            dataset_name,
+            rows,
+            period,
+            platform.python_version(),
+            platform.system(),
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "Enabled",
+            "Enabled",
+            "Enabled",
+            scheduler,
+            output_file
+        ]
+    }
+
+    os.makedirs("metadata", exist_ok=True)
+
+    df = pd.DataFrame(metadata)
+
+    df.to_csv(
+        "metadata/simulation_metadata.csv",
+        index=False
+    )
+
+    print("Metadata berhasil disimpan.")
